d278a9e896d89d29f57cec11047733050f2e3a43
Aman  Gangwar <subodhg013@gmail.com>
2026-02-11 10:11:51 +0530

    Implement protected routes and auth-based navbar

--- Smart-Library-Frontend/src/App.jsx
+++ Smart-Library-Frontend/src/App.jsx
@@ -1,43 +1,6 @@
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Login from "./pages/Login";
-// import Signup from "./pages/Signup";
-// import Books from "./pages/Books";
-// import Dashboard from "./pages/Dashboard";
-// import Payment from "./pages/Payment";
-// import ProtectedRoute from "./components/ProtectedRoute";
-// import Navbar from "./components/Navbar";
-
-// export default function App() {
-//   return (
-//     <BrowserRouter>
-//       {localStorage.getItem("token") && <Navbar />}
-//       <Routes>
-//         <Route path="/login" element={<Login />} />
-//         <Route path="/signup" element={<Signup />} />
-
-//         <Route path="/books" element={
-//           <ProtectedRoute><Books /></ProtectedRoute>
-//         } />
-
-//         <Route path="/dashboard" element={
-//           <ProtectedRoute><Dashboard /></ProtectedRoute>
-//         } />
-
-//         <Route path="/payment/:id" element={
-//           <ProtectedRoute><Payment /></ProtectedRoute>
-//         } />
-
-//         <Route path="*" element={<Login />} />
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
-
-
-
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 import Books from "./pages/Books";
 import Dashboard from "./pages/Dashboard";
@@ -51,9 +14,38 @@ function App() {
       <Navbar />
 
       <Routes>
-        <Route path="/books" element={<Books />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/payment/:id" element={<Payment />} />
+        {/* 🔁 Default redirect */}
+        <Route path="/" element={<Navigate to="/login" />} />
+
+        {/* 🔐 Protected routes */}
+        <Route
+          path="/books"
+          element={
+            <ProtectedRoute>
+              <Books />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/payment/:id"
+          element={
+            <ProtectedRoute>
+              <Payment />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* 🔓 Public routes */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
@@ -62,3 +54,4 @@ function App() {
 }
 
 export default App;
+
